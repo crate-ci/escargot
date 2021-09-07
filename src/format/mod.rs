111ad86b72519234abd795086e6bf7033a88d55d
Ed Page <eopage@gmail.com>
2021-09-07 18:43:21 -0500

    docs: Fix typo

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -79,7 +79,7 @@ pub struct Target<'a> {
     /// Kind of target ("bin", "example", "test", "bench", "lib")
     #[serde(borrow)]
     pub kind: Vec<CowStr<'a>>,
-    /// Almost the same as `kind`, except when an example is a library instad of an executable.
+    /// Almost the same as `kind`, except when an example is a library instead of an executable.
     /// In that case `crate_types` contains things like `rlib` and `dylib` while `kind` is `example`
     #[serde(default)]
     #[serde(borrow)]
